9566fa00de1b17043a99174953aa9dfe4c2ca952
Mukesh <mukeshkumarkarn@gmail.com>
2025-10-27 10:57:37 +0530

    New files

---
+++ vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use App\Models\login_log;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\ValidationException;
+use Stevebauman\Location\Facades\Location;
+
+
+trait AuthenticatesUsers
+{
+    use RedirectsUsers, ThrottlesLogins;
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            Session::flash('success', 'No Data Found.');
+            return back();
+        }
+        $ip = $request->ip();
+        $location = Location::get($ip);
+        if ($location && $location->latitude && $location->longitude) {
+            $latitude = $location->latitude;
+            $longitude = $location->longitude;
+            $user->update([
+                'current_latitude' => $latitude,
+                'current_longitute' => $longitude,
+            ]);
+        } else {
+            $user->update([
+                'current_latitude' => NULL,
+                'current_longitute' => NULL,
+            ]);
+        }
+
+        if (!$user) {
+            throw ValidationException::withMessages([
+                Session::flash('success', 'These credentials do not match our records.'),
+            ]);
+        }
+
+        if ($user->status == 4) {
+            throw ValidationException::withMessages([
+                Session::flash('success', 'Your account is blocked.'),
+            ]);
+        } elseif ($user->status == 3) {
+            throw ValidationException::withMessages([
+                Session::flash('success', 'Your account is deleted.'),
+            ]);
+        } elseif ($user->status == 1) {
+            $login_log = new login_log();
+            $login_log->user_id = $user->id;
+            $login_log->ip_address = $ip;
+            $login_log->country = $location->countryName;
+            $login_log->state = $location->regionName;
+            $login_log->city = $location->cityName;
+            $login_log->pincode = $location->postalCode;
+            $login_log->latitude = $location->latitude;
+            $login_log->longitute = $location->longitude;
+            $login_log->login_type = 'normal';
+            $login_log->loginDate = now();
+            $login_log->save();
+            if (
+                method_exists($this, 'hasTooManyLoginAttempts') &&
+                $this->hasTooManyLoginAttempts($request)
+            ) {
+                $this->fireLockoutEvent($request);
+                return $this->sendLockoutResponse($request);
+            }
+
+            if ($this->attemptLogin($request)) {
+                if ($request->hasSession()) {
+                    $request->session()->put('auth.password_confirmed_at', time());
+                }
+                return $this->sendLoginResponse($request);
+            }
+
+            $this->incrementLoginAttempts($request);
+            Session::flash('success', 'Worng Password.');
+            return $this->sendFailedLoginResponse($request);
+        } else {
+            // Status is neither blocked nor deleted nor active, assuming it's unverified
+            Session::flash('success', 'Email is not verified.');
+            return redirect()->to('login');
+        }
+    }
+
+
+
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+        ]);
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        return $this->guard()->attempt(
+            $this->credentials($request),
+            $request->boolean('remember')
+        );
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return $request->only($this->username(), 'password');
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+
+        $this->clearLoginAttempts($request);
+
+        if ($response = $this->authenticated($request, $this->guard()->user())) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect()->intended($this->redirectPath());
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        //
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'email';
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
+    }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
